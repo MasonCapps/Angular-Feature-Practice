34d452d4d509dbdaaf95529e2cf1d3f89430e2c3
Mason Capps <masoncapps42119@gmail.com>
2022-09-27 10:28:03 -0500

    Get data from component to main html page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TemplateBindingComponent } from './template-binding/template-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateBindingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/template-binding/template-binding.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-binding',
+  templateUrl: './template-binding.component.html',
+  styleUrls: ['./template-binding.component.css']
+})
+export class TemplateBindingComponent {
+
+  title = "Template - Data Binding Example";
+  
+
+}
